0f8190ed5afc684ea11cb52c80805d23b9ba71d8
GoldJay <jasonlynch10@hotmail.com>
2019-12-16 14:11:22 +0000

    - Added scaling to tesla projectile so its not massive

--- GD4SFMLGameWorld/Projectile.cpp
+++ GD4SFMLGameWorld/Projectile.cpp
@@ -27,6 +27,7 @@ Projectile::Projectile(ProjectileID type, const TextureHolder& textures)
 {
 	//Initializes animation for tesla bullet - Dylan Reilly
 	mFiringAnimation.setFrameSize(sf::Vector2i(128, 128));
+	mFiringAnimation.setScale(0.5f, 0.5f);
 	mFiringAnimation.setNumFrames(12);
 	mFiringAnimation.setDuration(sf::seconds(2));
 
